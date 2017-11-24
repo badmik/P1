4466e9f169a0a8d580c1a5c627910e0170b2ac2a
Mike Bader <badmik@gmail.com>
2017-11-24 16:45:58 +0100

    fetch

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -12,10 +12,22 @@ import {default as model} from "./model.js";
         showTasks();
     }
 
+    function getTasks() {
+        let url = 'http://localhost:3003/tasks';
+        return fetch(url).then(tasks => tasks.json());
+    }
+
+
+    function getTasks(id) {
+        let url = `http://localhost:3003/tasks/${id}\`;
+        return fetch(url).then(tasks => tasks.json());
+    }
+
+
 
     $(function () {
         TasksTemplateProcessor =  Handlebars.compile($("#tasks-list-template").html());
-        $(document).on("click", "input[send]", send());
+        $(document).on("click", "input[send]", ());
         $("#createTask").click(
             function () {
                 Taskmanager.addTask($("#name").val());
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const taskController = require('../public/javascripts/model.js');
 
-router.get('/:id/', (req, res) => {
+router.get('/tasks/:id/', (req, res) => {
     let id = req.params.id;
     taskController.getTask(id, (err, task) => {
         if (task) {
@@ -15,26 +15,26 @@ router.get('/:id/', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
+router.get('/tasks', (req, res) => {
     taskController.getTasks((err, tasks) => {
         res.json(tasks);
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/tasks/edit', (req, res) => {
     taskController.addTask((err, newTask) => {
         res.json(newTask);
     });
 });
 
-router.put('/:id/', (req, res) => {
+router.put('/tasks/:id/', (req, res) => {
     let id = req.params.id;
     taskController.updateTask(id, req.body, (err, updatedTask) => {
         res.json(updatedTask);
     });
 });
 
-router.delete('/:id/', (req, res) => {
+router.delete('/tasks/:id/', (req, res) => {
     let id = req.params.id;
     taskController.deleteTask(id, (err, task) => {
         res.json(task);
